3b36ff889826c75df7679f7932eb32d9827f19c2
Hadji Tejuco <hadji.tejuco@gmail.com>
2026-02-24 10:35:19 +0800

    Update sample1.cpp

--- Module 5/sample1.cpp
+++ Module 5/sample1.cpp
@@ -12,5 +12,19 @@ int main()
     cout << "Value at address stored in p: "<< *p <<endl;
     *p = 10;
     cout << "New value of v: " << v << endl;
+    
+    //float 
+    float pi = 3.14;
+    float *p2 = &pi;
+    cout << "Value of pi: " << *p2 << endl; //dereference
+    
+    //char 
+    char ch = 'A';
+    char *p3 = &ch;
+    cout << "Value of ch: " << *p3 << endl;
+    
+    
+    
+    
     return 0;
 }
